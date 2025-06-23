b6407f52adb4626226948b63d6b9e4fc701f4dc2
Юлгушев Рамиль <Ramil25.ramil25ramil@mail.ru>
2025-06-24 00:29:54 +0300

    Add: #1 созданы layouts

---
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+export function Sidebar() {
+  return (
+    <nav className="w-40 bg-teal-600">
+      <ul>
+        <li>MENU ITEM</li>
+        <li>MENU ITEM</li>
+        <li>MENU ITEM</li>
+        <li>MENU ITEM</li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const SITE_NAME = "Task Hub";
+export const SITE_DESCRIPTION = "Best place to manage your tasks";
